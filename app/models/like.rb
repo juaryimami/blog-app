1a10ee1abb4d35cf1b38f60d848be7d82b28fcd2
juharyimami <juharyimami2011@gmail.com>
2023-05-10 15:00:56 +0300

    data model processed

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,12 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :posts
+
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
+
+  def update_likes_counter
+    like_counter = post.likes.count
+    post.update(likes_counter: like_counter)
+  end
 end
