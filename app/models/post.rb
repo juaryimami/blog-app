6b93e6d7b8fb9374d86d685296a22bcdaa7b39fd
juharyimami <juharyimami2011@gmail.com>
2023-05-16 15:09:42 +0300

    linters error fixed

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,9 +6,9 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :title, length: { maximum: 250, too_long: '%<count> characters is the maximum allowed' }
   validates :comment_counter, numericality: { only_integer: true,
-                                               greater_than_or_equal_to: 0 }
+                                              greater_than_or_equal_to: 0 }
   validates :like_counter, numericality: { only_integer: true,
-                                            greater_than_or_equal_to: 0 }
+                                           greater_than_or_equal_to: 0 }
 
   after_save :update_posts_counter
 
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe User, type: :model do
   before :each do
     @user = User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher', post_counter: 0)
     @post = Post.new(title: 'Hi', text: 'My world', comment_counter: 0, like_counter: 0, author_id: 1)
-    @comment = Comment.new(post: @post, text: 'hello yimi', author_id:@user)
+    @comment = Comment.new(post: @post, text: 'hello yimi', author_id: @user)
   end
 
   it 'checks if attributes are valid' do
