6b63661c89297deaf244a7016c5e9ad4939f4a00
juharyimami <juharyimami2011@gmail.com>
2023-05-25 06:37:39 +0300

    issue fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_10_113827) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_23_090644) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,8 +36,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_10_113827) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "text"
-    t.integer "comment_counter"
-    t.integer "like_counter"
+    t.integer "comment_counter", default: 0
+    t.integer "like_counter", default: 0
     t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -46,11 +46,21 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_10_113827) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.text "photo"
+    t.string "photo"
     t.text "bio"
-    t.integer "post_counter"
+    t.integer "post_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.string "role", default: "default"
   end
 
   add_foreign_key "comments", "posts"
